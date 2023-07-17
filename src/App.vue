fced544f8ce066862034f3baf88b35ffc2a8ab93
Jared Hall <hall.jared.c@gmail.com>
2023-07-17 07:21:31 -0500

    a start

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import {RouterView } from 'vue-router'
+import AppHeader from "@/components/AppHeader.vue";
+</script>
+
+<template>
+
+  <AppHeader />
+  <div class="container">
+      <div class="container-content">&nbsp;</div>
+  </div>
+
+  <RouterView />
+</template>
+
+<style scoped lang="sass">
+
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import {RouterLink, RouterView, useRoute} from 'vue-router'
+
+const route = useRoute()
+
+</script>
+
+<template>
+
+  <div id="app-header">
+    <div class="row">
+      <div class="column" id="header-logo-column">
+        <strong class="header-title">Rook Takes</strong>
+      </div>
+
+      <div class="column" :class="{ selected: route.path === '/'}">
+        <RouterLink class="app-link" to="/">
+        Chessboard
+        </RouterLink>
+      </div>
+
+      <div class="column" :class="{ selected: route.path === '/visualization'}">
+        <RouterLink  class="app-link" to="/visualization">
+          Visualization
+        </RouterLink>
+      </div>
+
+
+      <div class="column" :class="{ selected: route.path === '/classic-games'}">
+        <RouterLink  class="app-link" to="/classic-games">
+        Classic Games
+        </RouterLink>
+      </div>
+    </div>
+  </div>
+
+
+
+</template>
+
+<style lang="sass">
+  @import "../style/header"
+</style>
